1a450e18a3656832e3c1a475763c9071d26bee1b
JoseHurtadoVGitHub <jose123.luis.jlh@gmail.com>
2025-10-06 22:19:04 -0500

    fixingURLLocalJson

--- src/Adapters/captaSkillTreeAdapter.ts
+++ src/Adapters/captaSkillTreeAdapter.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { BaseSkillTree } from '../Types/BaseSkillTree';
+import localBaseSkillTree from '../assets/BaseSkillTree.json'; // 👈 Import directo
 
 export async function fetchBaseSkillTree() {
   let data: BaseSkillTree;
   try {
     const MINECRAFT_SKILL_TREE_URL = import.meta.env.VITE_MINECRAFT_SKILL_TREE_URL;
+
+    if (!MINECRAFT_SKILL_TREE_URL) throw new Error('⚠️ Missing VITE_MINECRAFT_SKILL_TREE_URL');
+
     const { data: response } = await axios.get<BaseSkillTree>(MINECRAFT_SKILL_TREE_URL);
     data = response;
   } catch (error) {
     console.error(error);
-    const { data: response } = await axios.get<BaseSkillTree>('BaseSkillTree.json');
-    data = response;
+    return localBaseSkillTree;
   }
   return data;
 }
